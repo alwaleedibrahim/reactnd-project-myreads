0eee0ec33edd562b81612dc0e2e419b777eaa051
alwaleedibrahim <alwaleedibramohammed@gmail.com>
2022-04-25 20:37:38 +0200

    Fixed search isn't updating issue

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import * as BooksAPI from "../api/BooksAPI"
 
 function BookShelf(props) {
 
+    const [savedBooks, setSavedBooks] = useState(props.savedBooks)
+
     function handleSelect(e, book) {
         e.preventDefault()
         BooksAPI.update(book, e.target.value)
-        .then(() => props.updateBooks())
+            .then(() => props.updateBooks())
+        props.updateBooks().then(data => setSavedBooks(data))
+    }
+
+    function getShelf(id, shelf) {
+        if (savedBooks) {
+            const searchForBook = savedBooks.filter(item => item.id === id)
+            if (searchForBook[0]) {
+                return searchForBook[0]['shelf']
+            }
+            return 'none'
+        }
+        return shelf || 'none'
     }
 
     return (
@@ -20,7 +34,7 @@ function BookShelf(props) {
                                 <div className="book-top">
                                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks ? book.imageLinks.thumbnail : 'https://via.placeholder.com/128x193.png'}")` }}></div>
                                     <div className="book-shelf-changer">
-                                        <select value={book.shelf || 'none'} onChange={e => handleSelect(e, book)}>
+                                        <select value={getShelf(book.id, book.shelf)} onChange={e => handleSelect(e, book)}>
                                             <option value="move" disabled>Move to...</option>
                                             <option value="currentlyReading" >Currently Reading</option>
                                             <option value="wantToRead" >Want to Read</option>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom' 
+import React, {useState, useEffect} from 'react'
+import { Link, useSearchParams } from 'react-router-dom' 
 import BookShelf from './BookShelf'
 import * as BooksAPI from '../api/BooksAPI'
 
@@ -7,12 +7,29 @@ function Search () {
 
     const [books, setBooks] = useState([])
 
+    const [savedBooks, setSavedBooks] = useState([])
+
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    function updateBooks() {
+        return BooksAPI.getAll().then((data) => {
+            setSavedBooks(data)
+            return data
+        })
+    }
+
     function handleChange (e) {
         e.preventDefault()
-        e.target.value && 
-        BooksAPI.search(e.target.value)
-        .then(data => data? setBooks(data) : setBooks([]))
+        setSearchParams({q: e.target.value})
     }
+
+    useEffect(() => {
+        const query = searchParams.get('q')
+        query? (BooksAPI.search(query)
+        .then(data => data? setBooks(data) : setBooks([])))
+        : setBooks([])
+        updateBooks()
+    }, [searchParams])
 
     return (
         <div className="search-books">
@@ -21,11 +38,11 @@ function Search () {
                     <button className='close-search'>Close</button>
                 </Link>
                 <div className="search-books-input-wrapper">
-                    <input type="text" placeholder="Search by title or author" onChange={handleChange}/>
+                    <input type="text" placeholder="Search by title or author" onChange={handleChange} value={searchParams.get('q') || ""}/>
                 </div>
             </div>
             <div className="search-books-results">
-                {books[0]? <BookShelf title="Search results" books={books} shelf="all" updateBooks={() => {}}/> : <p>No Books Found</p>}
+                {books[0]? <BookShelf title="Search results" books={books} shelf="all" savedBooks={savedBooks} updateBooks={updateBooks}/> : searchParams.get('q')? <p>No Books Found</p> : ""}
             </div>
         </div>
     )
